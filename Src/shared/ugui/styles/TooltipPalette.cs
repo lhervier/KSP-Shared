e3e25648b6d128186cfb8c1f4fdfae1965f48f3d
lionel.hervier@laposte.net <lionel.hervier@laposte.net>
2026-06-10 17:48:16 +0200

    Tooltip

---
+++ Src/shared/ugui/styles/TooltipPalette.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+using static com.github.lhervier.ksp.shared.ugui.styles.Utils;
+
+namespace com.github.lhervier.ksp.shared.ugui.styles
+{
+    public static class TooltipPalette
+    {
+        public static readonly Color BackgroundColor = Rgb(26, 26, 26);
+        public static readonly Color AccentBorderColor = Rgb(74, 110, 32);
+        public static readonly Color TextColor = Rgb(232, 232, 232);
+    }
+}
